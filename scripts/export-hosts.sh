54d72f66dc2a02df101a9dc32464a9b77bc5584a
Skillmio <skillmiocfs@gmail.com>
2026-02-14 03:10:57 +0100

    Update export-hosts.sh

--- scripts/export-hosts.sh
+++ scripts/export-hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pihole-export-clean.sh - Export Pi-hole blocked domains with exempt filtering
-# to download this script wget https://raw.githubusercontent.com/skillmio/dns/master/scripts/exporter.sh
+# to download this script wget https://raw.githubusercontent.com/skillmio/dns/master/scripts/export-hosts.sh
 # chmod +x exporter.sh
 
 set -e  # Exit on error
